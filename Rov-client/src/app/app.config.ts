77e023c442fc270195c2bd390ad905575ada45ab
Vertexx11 <atiwitkisantia@gmail.com>
2026-02-18 18:46:52 +0700

    fix: remove hydration and server config files

--- Rov-client/src/app/app.config.ts
+++ Rov-client/src/app/app.config.ts
@@ -3,7 +3,6 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http'; 
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { loadingInterceptor } from './_interceptors/loading-interceptor';
 import { errorInterceptor } from './_interceptors/error-interceptor';
 import { jwtInterceptor } from './_interceptors/jwt-interceptor';
@@ -12,10 +11,9 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),  
     provideRouter(routes),
-    
-    provideHttpClient(withFetch()), 
-
-    provideClientHydration(withEventReplay()),
-    provideHttpClient( withInterceptors([ loadingInterceptor , errorInterceptor, jwtInterceptor ]) ),
+    provideHttpClient(
+      withFetch(), 
+      withInterceptors([loadingInterceptor, errorInterceptor, jwtInterceptor])
+    ),
   ]
 };
