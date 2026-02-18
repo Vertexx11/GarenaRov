547c2a1fb8e9e78ebe8b76b192cdd8abf928f410
Vertexx11 <atiwitkisantia@gmail.com>
2026-02-19 00:29:31 +0700

    new

--- Rov-client/src/app/_models/brawler.ts
+++ Rov-client/src/app/_models/brawler.ts
@@ -1,27 +1,28 @@
 export interface Brawler {
     id: number;
-    displayname: string,
-    avatar: string,
+    username: string;
+    display_name: string;
+    avatar_url?: string;
     total_points: number;
-    mission_success_count: number,
-    mission_join_count: number
+    mission_success_count: number;
+    mission_joined_count: number;
 }
 
 export interface Passport {
-    token_type: string,
-    access_token: string,
-    expires_in: number,
-    display_name: string,
-    arvatar_url?: string
+    token_type: string;
+    access_token: string;
+    expires_in: number;
+    display_name: string;
+    avatar_url?: string;
 }
 
 export interface LoginData {
-    username: string
-    password: string
+    username: string;
+    password: string;
 }
 
 export interface RegisterData {
-    username: string,
-    password: string,
-    display_name: string
+    username: string;
+    password: string;
+    display_name: string;
 }
